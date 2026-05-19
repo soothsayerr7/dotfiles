364e054b7cb971673856147d38de9b0e35cfd816
Eduardo <eduardo.costa.xyz@gmail.com>
2026-05-19 00:36:33 -0300

    fix(mangohud): move config to misc folder due to conflicts with flatpak conventions

--- TODO.md
+++
@@ -1,3 +0,0 @@
-[x] Expand on new hyprland option and remove/simplify created .sh scripts where possible.
-[ ] Implement bash scripts using hyprland lua instead.
-[ ] Correct helium rose-pine theme tab colors.
--- common/hypr/scripts/fullscreen.sh
+++ common/hypr/scripts/fullscreen.sh
@@ -5,13 +5,13 @@ ws=$(echo "$window" | jq -r '.workspace.id')
 fs_state=$(echo "$window" | jq -r '.fullscreenClient')
 
 if [[ ! "$fs_state" -eq 3 ]]; then
-  hyprctl dispatch "hl.dsp.layout('colresize 1.0')"
-  hyprctl dispatch "hl.dsp.window.fullscreen_state({ internal = 0, client = 3 })"
   hyprctl dispatch "hl.dsp.window.set_prop({ prop = 'border_size', value = 0 })"
   hyprctl eval "hl.workspace_rule({ workspace = $ws, gaps_out = 0 })"
+  hyprctl dispatch "hl.dsp.layout('colresize 1.0')"
+  hyprctl dispatch "hl.dsp.window.fullscreen_state({ internal = 0, client = 3 })"
 
 else
-  hyprctl dispatch "hl.dsp.window.fullscreen_state({ internal = 0, client = 0 })"
-  hyprctl dispatch "hl.dsp.window.set_prop({ prop = 'border_size', value = 2 })"
   hyprctl eval "hl.workspace_rule({ workspace = $ws, gaps_out = 6 })"
+  hyprctl dispatch "hl.dsp.window.set_prop({ prop = 'border_size', value = 2 })"
+  hyprctl dispatch "hl.dsp.window.fullscreen_state({ internal = 0, client = 0 })"
 fi
